2ca0aa8ab516b0f634291292e1973b9b164b4e75
julio177 <julioc.aguilarv@gmail.com>
2017-10-18 09:50:44 -0500

    Comienza funcion obstacle avoidance del servidor

--- server.py
+++ server.py
@@ -1,22 +1,23 @@
 from flask import Flask
 import json
+import re # modulo de regex
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=["GET"])
 def hello():
     '''Funcion de testing para servidor'''
     return "Hello World!"
 
-@app.route("/followLeader/<course>/<teamCode>", methods=['GET'])
+@app.route("/followLeader/<course>/<teamCode>", methods=["GET"])
 def follow_leader(course, teamCode):
     '''Funcion para iniciar follow de leader challenge'''
-    return json.dumps({'success':True, 'course': course}), 200, {'ContentType':'application/json'}
+    return json.dumps({"success":True, "course": course}), 200, {"ContentType":"application/json"}
 
 @app.route("/heartbeat/<course>/<teamCode>", methods=['POST'])
 def heartbeat(course, teamCode):
     '''Funcion heartbeat para log de estatus de bote'''
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    return json.dumps({"success":True}), 200, {"ContentType":"application/json"}
 
 @app.route("/run/start/<course>/<teamCode>", methods=['POST'])
 def run_start(course, teamCode):
@@ -33,4 +34,28 @@ def docking(course, teamCode):
     '''Funcion para mandar docking al bote'''
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
-
+@app.route("/obstacleAvoidance/<course>/<teamCode>", methods=['GET'])
+def obstacleAvoidance(course=None, teamCode=None):
+    # validar llamada
+    if course == None or teamCode == None:
+        return json.dumps({"error": True, "msg": "course or teamCode None"}), 400, {"ContentType":"application/json"}
+    
+    pattern = re.compile("[a-zA-Z]{2,5}")
+    # validar curso
+    if course == "courseA":
+        if pattern.match(teamCode):
+            print("hello")
+        else:
+            return json.dumps({"error": True, "msg": "cannot find team"}), 404, {"ContentType":"application/json"}
+    elif course == "courseB":
+        if pattern.match(teamCode):
+            print("hello")
+        else:
+            return json.dumps({"error": True, "msg": "cannot find team"}), 404, {"ContentType":"application/json"}
+    elif course == "openTest":
+        if pattern.match(teamCode):
+            print("hello")
+        else:
+            return json.dumps({"error": True, "msg": "cannot find team"}), 404, {"ContentType":"application/json"}
+    else:
+        return json.dumps({"error": True, "msg": "Cannot find course"}), 404, {"ContentType":"application/json"}

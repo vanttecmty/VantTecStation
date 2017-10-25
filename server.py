e9fb79f2c4c20e41faa22f51f1d0a7f569a9748d
julio177 <julioc.aguilarv@gmail.com>
2017-10-24 23:01:52 -0500

    Crea funcion dockingBay y obstacleAvoidance

--- server.py
+++ server.py
@@ -38,24 +38,135 @@ def docking(course, teamCode):
 def obstacleAvoidance(course=None, teamCode=None):
     # validar llamada
     if course is None or teamCode is None:
-        return json.dumps({"error": True, "msg": "course or teamCode None"}), 400, {"ContentType":"application/json"}
+        return json.dumps({
+            "error": True,
+            "msg": "course or teamCode None"
+        }), 400, {"ContentType":"application/json"}
 
     pattern = re.compile("[a-zA-Z]{2,5}")
     # validar curso
     if course == "courseA":
         if pattern.match(teamCode):
-            print("hello")
+            # status 200
+            return json.dumps({
+                "gateCode": "(1, X)"
+            }), 200, {"ContentType":"application/json"}
         else:
-            return json.dumps({"error": True, "msg": "cannot find team"}), 404, {"ContentType":"application/json"}
+            # error
+            return json.dumps({
+                "error": True,
+                "msg": "cannot find team"
+            }), 404, {"ContentType":"application/json"}
+
     elif course == "courseB":
         if pattern.match(teamCode):
-            print("hello")
+            # status 200
+            return json.dumps({
+                "gateCode": "(2, Y)"
+            }), 200, {"ContentType":"application/json"}
         else:
-            return json.dumps({"error": True, "msg": "cannot find team"}), 404, {"ContentType":"application/json"}
+            # error
+            return json.dumps({
+                "error": True,
+                "msg": "cannot find team"
+            }), 404, {"ContentType":"application/json"}
+
     elif course == "openTest":
         if pattern.match(teamCode):
-            print("hello")
+            # status 200
+            return json.dumps({
+                "gateCode": "(3, Z)"
+            }), 200, {"ContentType":"application/json"}
         else:
-            return json.dumps({"error": True, "msg": "cannot find team"}), 404, {"ContentType":"application/json"}
+            # error
+            return json.dumps({
+                "error": True,
+                "msg": "cannot find team"
+            }), 404, {"ContentType":"application/json"}
     else:
-        return json.dumps({"error": True, "msg": "Cannot find course"}), 404, {"ContentType":"application/json"}
+        return json.dumps({
+            "error": True,
+            "msg": "Cannot find course"
+        }), 404, {"ContentType":"application/json"}
+
+@app.route("/automatedDocking/<course>/<teamCode>", methods=["GET"])
+def automatedDocking(course=None, teamCode=None):
+    # validar llamada
+    if course is None or teamCode is None:
+        return json.dumps({
+            "error": True,
+            "msg": "course or teamCode None"
+        }), 400, {"ContentType":"application/json"}
+
+    pattern = re.compile("[a-zA-Z]{2,5}")
+    # validar curso
+    if course == "courseA":
+        if pattern.match(teamCode):
+            # status 200
+            return json.dumps({
+                "dockingBaySequence": [
+                    {
+                        "symbol": "cruciform",
+                        "color": "red"
+                    },
+                    {
+                        "symbol": "cruciform",
+                        "color": "red"
+                    }
+                ]
+            }), 200, {"ContentType":"application/json"}
+        else:
+            # error
+            return json.dumps({
+                "error": True,
+                "msg": "cannot find team"
+            }), 404, {"ContentType":"application/json"}
+
+    elif course == "courseB":
+        if pattern.match(teamCode):
+            # status 200
+            return json.dumps({
+                "dockingBaySequence": [
+                    {
+                        "symbol": "triangle",
+                        "color": "green"
+                    },
+                    {
+                        "symbol": "triangle",
+                        "color": "green"
+                    }
+                ]
+            }), 200, {"ContentType":"application/json"}
+        else:
+            # error
+            return json.dumps({
+                "error": True,
+                "msg": "cannot find team"
+            }), 404, {"ContentType":"application/json"}
+
+    elif course == "openTest":
+        if pattern.match(teamCode):
+            # status 200
+            return json.dumps({
+                "dockingBaySequence": [
+                    {
+                        "symbol": "circle",
+                        "color": "blue"
+                    },
+                    {
+                        "symbol": "circle",
+                        "color": "blue"
+                    }
+                ]
+            }), 200, {"ContentType":"application/json"}
+        else:
+            # error
+            return json.dumps({
+                "error": True,
+                "msg": "cannot find team"
+            }), 404, {"ContentType":"application/json"}
+    else:
+        return json.dumps({
+            "error": True,
+            "msg": "Cannot find course"
+        }), 404, {"ContentType":"application/json"}

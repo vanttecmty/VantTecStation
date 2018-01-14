d246c090b8454409f9afd7473e91c477585d722c
julio177 <julioc.aguilarv@gmail.com>
2018-01-14 16:09:29 -0600

    Completa subida de imagenes con ids unicos y regresa id con length del archivo

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -1,17 +1,47 @@
-from flask import Flask
+from flask import Flask, Response, make_response
 from werkzeug.utils import secure_filename
+from flask import jsonify, flash # modulo para regresar jsons
+import os # acceso al sistema
+import uuid # modulo para ids unicos
+import sys # usar system path
 # http://flask.pocoo.org/docs/0.12/quickstart/
 
-UPLOAD_FOLDER = '/static'
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static\\uploads')
 ALLOWED_EXTENSIONS = set(['txt', 'png', 'jpg', 'jpeg'])
 
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+def eprint(*args, **kwargs):
+    '''Funcion de ayuda para hacer print a consola'''
+    print(*args, file=sys.stderr, **kwargs)
+
 def allowed_file(filename):
-    '''Checar si la extension del archiv es valida'''
+    '''Checar si la extension del archivo es valida'''
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def upload_file(file):
+    # validar que la llamada tiene archivo
+    if file.filename == '':
+        flash("No se selecciono archivo")
+        return jsonify(error="No se selecciono archivo")
+
+    # validar extension de archivo
+    if file and allowed_file(file.filename):
+        # crear id para filename
+        filename = str(uuid.uuid4())
+        extension = '.' + file.filename.rsplit('.', 1)[1].lower()
+        # cambiar filename
+        file.filename = filename + extension
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(file_path)
+        response = make_response(jsonify(id=filename))
+        response.headers['Content-Type'] = 'application/json'
+        response.headers['Content-Length'] = os.stat(file_path).st_size
+        return response
+    return jsonify(error="Subir archivo de nuevo")
 
 from server.functions import *
 from server.automatedDocking import automatedDocking
--- server/server/interopImg.py
+++ server/server/interopImg.py
@@ -1,9 +1,11 @@
-from server import app # app para las rutas
+from server import app, upload_file # app para las rutas
 import json # modulo de jsons
 import re # modulo de regex
-from flask import request, after_this_request, jsonify # ver requests recibidas en flask
+from flask import request, after_this_request, jsonify, flash # ver requests recibidas en flask
 import sys # usar system path
 
+
+COURSES = set(['courseA', 'courseB', 'openTest'])
 
 def eprint(*args, **kwargs):
     '''Funcion de ayuda para hacer print a consola'''
@@ -13,41 +15,22 @@ def eprint(*args, **kwargs):
 def interopImg(course=None, teamCode=None):
     # validar llamada
     if course is None or teamCode is None:
-        return "Not OK"
+        return jsonify(result="Not OK", error="Teamcode o course es None")
 
+    if 'file' not in request.files:
+        flash('No incluye archivo')
+        return jsonify(error="No incluye archivo")
+
+    file = request.files['file']
     pattern = re.compile("[a-zA-Z]{2,5}$")
+
     # validar curso
-    if course == "courseA":
-        # validate team
+    if course in COURSES:
+        # validar team
         if pattern.match(teamCode):
             # status 200
-            return jsonify(status=200)
+            return upload_file(file)
         else:
             # error
-            return "404"
-
-    elif course == "courseB":
-        # validate team
-        if pattern.match(teamCode):
-            # status 200
-            return "200"
-        else:
-            # error
-            return "404"
-
-    elif course == "openTest":
-        # validate team
-        if pattern.match(teamCode):
-            # status 200
-            return "200"
-        else:
-            # error
-            return "500"
-    else:
-        return "Nope"
-
-    @after_this_request
-    def add_header(response):
-        response.headers['Content-Type'] = 'Text'
-        response.headers['Content-Length'] = '1500'
-        return response
+            return jsonify(status=403, error="Codigo de equipo no permitido")
+    return jsonify(status=404, error="Curso no encontrado")

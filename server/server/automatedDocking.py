1cb28dfc96ac6fcaf568ea122c4f3537a1111699
julio177 <julioc.aguilarv@gmail.com>
2018-01-22 14:18:29 -0600

    Refactor

--- server/server/automatedDocking.py
+++ server/server/automatedDocking.py
@@ -1,88 +1,36 @@
-from server import app
+from server import app, COURSES
 import json
 import re # modulo de regex
+from flask import jsonify
+from random import randint
+
+COLORS = ["red", "green", "blue"]
+SYMBOLS = ["cruciform", "triangle", "circle"]
+COLORS_LEN = len(COLORS)
+SYM_LEN = len(SYMBOLS)
 
 @app.route("/automatedDocking/<course>/<teamCode>", methods=["GET"])
 def automatedDocking(course=None, teamCode=None):
+    '''Funcion con curso para automated docking'''
     # validar llamada
     if course is None or teamCode is None:
-        return json.dumps({
-            "error": True,
-            "msg": "course or teamCode None"
-        }), 400, {"ContentType":"application/json"}
+        return jsonify(status=400, msg="Request is malformed")
 
     pattern = re.compile("[a-zA-Z]{2,5}$")
     # validar curso
-    if course == "courseA":
+    if course in COURSES:
         # validate team
         if pattern.match(teamCode):
-            # status 200
             return json.dumps({
                 "dockingBaySequence": [
                     {
-                        "symbol": "cruciform",
-                        "color": "red"
+                        "symbol": SYMBOLS[randint(0, SYM_LEN - 1)],
+                        "color": COLORS[randint(0, COLORS_LEN - 1)]
                     },
                     {
-                        "symbol": "cruciform",
-                        "color": "red"
+                        "symbol": SYMBOLS[randint(0, SYM_LEN - 1)],
+                        "color": COLORS[randint(0, COLORS_LEN - 1)]
                     }
                 ]
             }), 200, {"ContentType":"application/json"}
-        else:
-            # error
-            return json.dumps({
-                "error": True,
-                "msg": "cannot find team"
-            }), 404, {"ContentType":"application/json"}
-
-    elif course == "courseB":
-        # validate team
-        if pattern.match(teamCode):
-            # status 200
-            return json.dumps({
-                "dockingBaySequence": [
-                    {
-                        "symbol": "triangle",
-                        "color": "green"
-                    },
-                    {
-                        "symbol": "triangle",
-                        "color": "green"
-                    }
-                ]
-            }), 200, {"ContentType":"application/json"}
-        else:
-            # error
-            return json.dumps({
-                "error": True,
-                "msg": "cannot find team"
-            }), 404, {"ContentType":"application/json"}
-
-    elif course == "openTest":
-        # validate team
-        if pattern.match(teamCode):
-            # status 200
-            return json.dumps({
-                "dockingBaySequence": [
-                    {
-                        "symbol": "circle",
-                        "color": "blue"
-                    },
-                    {
-                        "symbol": "circle",
-                        "color": "blue"
-                    }
-                ]
-            }), 200, {"ContentType":"application/json"}
-        else:
-            # error
-            return json.dumps({
-                "error": True,
-                "msg": "cannot find team"
-            }), 404, {"ContentType":"application/json"}
-    else:
-        return json.dumps({
-            "error": True,
-            "msg": "Cannot find course"
-        }), 404, {"ContentType":"application/json"}
+    return jsonify(status=404, msg="Cannot find team or course")

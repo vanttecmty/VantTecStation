a8d70b756079c8684efa6889337f7c5fc66b3b54
julio177 <julioc.aguilarv@gmail.com>
2018-01-22 14:19:28 -0600

    Funcion de reporte de imagen

--- server/server/interopImg.py
+++ server/server/interopImg.py
@@ -1,18 +1,12 @@
-from server import app, upload_file # app para las rutas
+from server import app, upload_file, eprint, COURSES # app para las rutas
 import json # modulo de jsons
 import re # modulo de regex
 from flask import request, after_this_request, jsonify, flash # ver requests recibidas en flask
-import sys # usar system path
 
-
-COURSES = set(['courseA', 'courseB', 'openTest'])
-
-def eprint(*args, **kwargs):
-    '''Funcion de ayuda para hacer print a consola'''
-    print(*args, file=sys.stderr, **kwargs)
 
 @app.route("/interop/image/<course>/<teamCode>", methods=["POST"])
 def interopImg(course=None, teamCode=None):
+    '''Funcion para subir imagen'''
     # validar llamada
     if course is None or teamCode is None:
         return jsonify(result="Not OK", error="Teamcode o course es None")
@@ -34,3 +28,18 @@ def interopImg(course=None, teamCode=None):
             # error
             return jsonify(status=403, error="Codigo de equipo no permitido")
     return jsonify(status=404, error="Curso no encontrado")
+
+@app.route("/interop/report/<course>/<teamCode>", methods=["POST"])
+def reportImg(course=None, teamCode=None):
+    '''Funcion para reportar forma de imagen'''
+    if course is None or teamCode is None:
+        return jsonify(status=400, message="Request is malformed")
+
+    pattern = re.compile("[a-zA-Z]{2,5}$")
+
+    if course in COURSES:
+        if pattern.match(teamCode):
+            eprint(request.data["shape"])
+            return jsonify(success=200)
+    return jsonify(status=404, message="Cannot find course or team")
+

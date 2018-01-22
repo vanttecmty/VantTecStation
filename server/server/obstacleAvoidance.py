f690f91c4ac5f4b6623a221f1013f8f02a9ea075
julio177 <julioc.aguilarv@gmail.com>
2018-01-22 14:32:00 -0600

    Refactor de obstacle avoidance

--- server/server/obstacleAvoidance.py
+++ server/server/obstacleAvoidance.py
@@ -1,61 +1,31 @@
-from server import app # app para agregar rutas
+from server import app, COURSES # app para agregar rutas
 import json # modulo de json
 import re # modulo de regex
+from flask import jsonify
+from random import randint
+
+NUMS = ["1", "2", "3"]
+LETTERS = ["X", "Y", "Z"]
+NUMS_LEN = len(NUMS)
+LETTERS_LEN = len(LETTERS)
 
 @app.route("/obstacleAvoidance/<course>/<teamCode>", methods=["GET"])
 def obstacleAvoidance(course=None, teamCode=None):
+    '''Funcion para evadir obstaculo por un gate'''
     # validar llamada
     if course is None or teamCode is None:
-        return json.dumps({
-            "error": True,
-            "msg": "course or teamCode None"
-        }), 400, {"ContentType":"application/json"}
+        return jsonify(status=400, msg="Request is malformed")
 
     pattern = re.compile("[a-zA-Z]{2,5}$")
     # validar curso
-    if course == "courseA":
+    if course in COURSES:
         # validate team
         if pattern.match(teamCode):
             # status 200
+            num = NUMS[randint(0, NUMS_LEN - 1)]
+            letter = LETTERS[randint(0, LETTERS_LEN - 1)]
+            gate_code = "(" + num + "," + letter + ")"
             return json.dumps({
-                "gateCode": "(1, X)"
+                "gateCode": gate_code
             }), 200, {"ContentType":"application/json"}
-        else:
-            # error
-            return json.dumps({
-                "error": True,
-                "msg": "cannot find team"
-            }), 404, {"ContentType":"application/json"}
-
-    elif course == "courseB":
-        # validate team
-        if pattern.match(teamCode):
-            # status 200
-            return json.dumps({
-                "gateCode": "(2, Y)"
-            }), 200, {"ContentType":"application/json"}
-        else:
-            # error
-            return json.dumps({
-                "error": True,
-                "msg": "cannot find team"
-            }), 404, {"ContentType":"application/json"}
-
-    elif course == "openTest":
-        # validate team
-        if pattern.match(teamCode):
-            # status 200
-            return json.dumps({
-                "gateCode": "(3, Z)"
-            }), 200, {"ContentType":"application/json"}
-        else:
-            # error
-            return json.dumps({
-                "error": True,
-                "msg": "cannot find team"
-            }), 404, {"ContentType":"application/json"}
-    else:
-        return json.dumps({
-            "error": True,
-            "msg": "Cannot find course"
-        }), 404, {"ContentType":"application/json"}
+    return jsonify(status=404, msg="Cannot find team or course")

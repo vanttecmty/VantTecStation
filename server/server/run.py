43c111cb124ba65078ebfd4b9db606f8ca8bae27
julio177 <julioc.aguilarv@gmail.com>
2018-01-22 20:15:30 -0600

    Completa funcion end de curso

--- server/server/run.py
+++ server/server/run.py
@@ -13,4 +13,14 @@ def start(course, teamCode):
             return make_response(jsonify(success=True), 200)
     return make_response(jsonify(success=False, msg="Cannot find team or course"), 404)
 
+@app.route("/run/end/<course>/<teamCode>", methods=["POST"])
+def end(course, teamCode):
+    '''Funcion para terminar el run del concurso'''
+    if course is None or teamCode is None:
+        return make_response(jsonify(success=False, msg="Request is malformed"), 400)
 
+    if course in COURSES:
+        if TEAM_PATTERN.mathc(teamCode):
+            eprint("End")
+            return make_response(jsonify(success=True), 200)
+    return make_response(jsonify(success=False, msg="Cannot find team or course"), 404)

b0df71dde8e38a71ccf1b51bf63aab5f8d470b51
julio177 <julioc.aguilarv@gmail.com>
2018-01-22 18:30:29 -0600

    Termina pinger

--- server/server/pinger.py
+++ server/server/pinger.py
@@ -13,13 +13,13 @@ def pingLoc(course, teamCode):
         return jsonify(status=400, msg="Request is malformed")
 
     pattern = re.compile("[a-zA-Z]{2,5}$")
-    
+
     if course in COURSES:
         if pattern.match(teamCode):
             buoyColor1Flag = request.data.get("buoyColor1", 0) in COLORS
             buoyColor2Flag = request.data.get("buoyColor2", 0) in COLORS
             frequency1Flag = 25 <= int(request.data.get("frequency1", 0)) <= 40
-            frequency2Flag = 25 <= int(request.data.get("frequency2", 0)) <= 40            
+            frequency2Flag = 25 <= int(request.data.get("frequency2", 0)) <= 40
             if buoyColor1Flag and buoyColor2Flag and frequency1Flag and frequency2Flag:
                 return jsonify(success=True)
             return jsonify(success=False)

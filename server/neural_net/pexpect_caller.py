9f2f86dc6f1bac745f46004eba517df5f589351f
julio177 <julioc.aguilarv@gmail.com>
2018-05-10 16:43:47 -0600

    Pruebas

--- server/neural_net/pexpect_caller.py
+++ server/neural_net/pexpect_caller.py
@@ -1,18 +1,20 @@
 import pexpect
 from pexpect import popen_spawn
+import sys
 
 def main():
-    child = popen_spawn.PopenSpawn('a.exe')
+    child = popen_spawn.PopenSpawn('./a.exe')
+    #child.logfile = sys.stdout
     child.expect('Enter name: \n')
     child.sendline('Luke')
-    child.expect('Tu nombre es Luke')
-    child.expect('Enter name: ')
+    child.expect('Tu nombre es Luke\n')
+    child.expect('Enter name: \n')
     child.sendline('Mike')
-    child.expect('Tu nombre es Mike')
-    child.expect('Enter name: ')
+    child.expect('Tu nombre es Mike\n')
+    child.expect('Enter name: \n')
     child.sendline('Jonathan')
-    child.expect('Tu nombre es Jonathan')
-    child.expect('Enter name: ')
+    child.expect('Tu nombre es Jonathan\n')
+    child.expect('Enter name: \n')
     child.sendline('Exit')
 
 main()
